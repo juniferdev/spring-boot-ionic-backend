5f8a8ce7ce7729128e13f008c2ffa12da58f8bf4
Vanderlei Junior <juniferdev@gmail.com>
2018-06-06 15:54:18 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/juniferdev/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.juniferdev.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juniferdev.cursomc.domain.Cliente;
+import com.juniferdev.cursomc.repositories.ClienteRepository;
+import com.juniferdev.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+		
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
