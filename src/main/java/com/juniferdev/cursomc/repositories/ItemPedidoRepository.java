b6ff19d5243661e19b73bd2057f889b19aaef10a
Vanderlei Junior <juniferdev@gmail.com>
2018-06-07 00:56:52 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/juniferdev/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.juniferdev.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.juniferdev.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+	
+}
